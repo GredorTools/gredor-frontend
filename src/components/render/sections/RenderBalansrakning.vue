dadd8843c2746c6822df2949ff38a4ffdec5f817
stafak <stafak@kth.se>
2025-08-02 17:56:49 +0200

    Bättre marginaler i utskrifter

--- src/components/render/sections/RenderBalansrakning.vue
+++ src/components/render/sections/RenderBalansrakning.vue
@@ -20,37 +20,39 @@ defineProps<{
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th scope="col"><h2>Balansräkning</h2></th>
-        <th class="not-container" scope="col">Not</th>
-        <th class="value-container" scope="col">
-          {{ arsredovisning.verksamhetsarNuvarande.slutdatum }}
-        </th>
-        <th
-          v-if="arsredovisning.verksamhetsarTidigare.length > 0"
-          class="value-container"
-          scope="col"
-        >
-          {{ arsredovisning.verksamhetsarTidigare[0].slutdatum }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <RenderBelopprad
-        v-for="belopprad in arsredovisning.balansrakning"
-        :key="belopprad.taxonomyItemName"
-        :belopprad="belopprad"
-        :comparable-num-previous-years="
-          Math.min(arsredovisning.verksamhetsarTidigare.length, 1)
-        "
-        :taxonomy-manager="taxonomyManager"
-        comparable-allow-not
-        string-show-header
-      />
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col"><h2>Balansräkning</h2></th>
+          <th class="not-container" scope="col">Not</th>
+          <th class="value-container" scope="col">
+            {{ arsredovisning.verksamhetsarNuvarande.slutdatum }}
+          </th>
+          <th
+            v-if="arsredovisning.verksamhetsarTidigare.length > 0"
+            class="value-container"
+            scope="col"
+          >
+            {{ arsredovisning.verksamhetsarTidigare[0].slutdatum }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <RenderBelopprad
+          v-for="belopprad in arsredovisning.balansrakning"
+          :key="belopprad.taxonomyItemName"
+          :belopprad="belopprad"
+          :comparable-num-previous-years="
+            Math.min(arsredovisning.verksamhetsarTidigare.length, 1)
+          "
+          :taxonomy-manager="taxonomyManager"
+          comparable-allow-not
+          string-show-header
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style lang="scss" scoped>
--- src/components/render/sections/RenderResultatrakning.vue
+++ src/components/render/sections/RenderResultatrakning.vue
@@ -43,47 +43,49 @@ function getRenderAsLevelForBelopprad(
 </script>
 
 <template>
-  <table>
-    <thead>
-      <tr>
-        <th scope="col"><h2>Resultaträkning</h2></th>
-        <th class="not-container" scope="col">Not</th>
-        <th class="value-container" scope="col">
-          {{ arsredovisning.verksamhetsarNuvarande.startdatum }}<br />
-          –{{ arsredovisning.verksamhetsarNuvarande.slutdatum }}
-        </th>
-        <th
-          v-if="arsredovisning.verksamhetsarTidigare.length > 0"
-          class="value-container"
-          scope="col"
-        >
-          {{ arsredovisning.verksamhetsarTidigare[0].startdatum }}<br />
-          –{{ arsredovisning.verksamhetsarTidigare[0].slutdatum }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <RenderBelopprad
-        v-for="belopprad in arsredovisning.resultatrakning.filter((b) => {
-          return ![
-            // Dessa vill vi inte ska renderas
-            'se-gen-base:RorelseresultatAbstract',
-            'se-gen-base:SkatterAbstract',
-          ].includes(b.taxonomyItemName);
-        })"
-        :key="belopprad.taxonomyItemName"
-        :belopprad="belopprad"
-        :comparable-num-previous-years="
-          Math.min(arsredovisning.verksamhetsarTidigare.length, 1)
-        "
-        :render-as-level="getRenderAsLevelForBelopprad(belopprad)"
-        :taxonomy-manager="taxonomyManager"
-        comparable-allow-not
-        monetary-show-balance-sign
-        string-show-header
-      />
-    </tbody>
-  </table>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th scope="col"><h2>Resultaträkning</h2></th>
+          <th class="not-container" scope="col">Not</th>
+          <th class="value-container" scope="col">
+            {{ arsredovisning.verksamhetsarNuvarande.startdatum }}<br />
+            –{{ arsredovisning.verksamhetsarNuvarande.slutdatum }}
+          </th>
+          <th
+            v-if="arsredovisning.verksamhetsarTidigare.length > 0"
+            class="value-container"
+            scope="col"
+          >
+            {{ arsredovisning.verksamhetsarTidigare[0].startdatum }}<br />
+            –{{ arsredovisning.verksamhetsarTidigare[0].slutdatum }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <RenderBelopprad
+          v-for="belopprad in arsredovisning.resultatrakning.filter((b) => {
+            return ![
+              // Dessa vill vi inte ska renderas
+              'se-gen-base:RorelseresultatAbstract',
+              'se-gen-base:SkatterAbstract',
+            ].includes(b.taxonomyItemName);
+          })"
+          :key="belopprad.taxonomyItemName"
+          :belopprad="belopprad"
+          :comparable-num-previous-years="
+            Math.min(arsredovisning.verksamhetsarTidigare.length, 1)
+          "
+          :render-as-level="getRenderAsLevelForBelopprad(belopprad)"
+          :taxonomy-manager="taxonomyManager"
+          comparable-allow-not
+          monetary-show-balance-sign
+          string-show-header
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style lang="scss" scoped>
