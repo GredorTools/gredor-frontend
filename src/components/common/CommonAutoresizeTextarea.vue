2b5041590f62f81595ea4ae9836befebca360b30
stafak <stafak@kth.se>
2026-01-11 17:05:12 +0100

    Justera storlek på textarea automatiskt

---
+++ src/components/common/CommonAutoresizeTextarea.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { computed, type TextareaHTMLAttributes } from "vue";
+import { useTextareaAutosize } from "@vueuse/core";
+
+defineProps</* @vue-ignore */ TextareaHTMLAttributes>();
+
+const input = defineModel<string>();
+
+const { textarea } = useTextareaAutosize({
+  input: computed(() => input.value || ""),
+});
+</script>
+
+<template>
+  <textarea ref="textarea" v-model="input" />
+</template>
+
+<style lang="scss" scoped>
+textarea {
+  resize: none;
+  transition: none;
+  overflow-y: hidden;
+}
+</style>
