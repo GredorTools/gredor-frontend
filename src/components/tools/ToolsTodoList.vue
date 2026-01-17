1076450b0c5effd8583653791f09f86b2deaabb7
stafak <stafak@kth.se>
2026-01-17 17:07:56 +0100

    Tydligare UX för att stänga "att åtgärda"-vyn

--- src/components/tools/ToolsTodoList.vue
+++ src/components/tools/ToolsTodoList.vue
@@ -5,7 +5,7 @@
  * färdigställas.
  */
 
-import { computed, onMounted, useTemplateRef } from "vue";
+import { computed, onMounted, ref, useTemplateRef, watch } from "vue";
 import { Popover } from "bootstrap";
 import ToolsTodoListPopover from "@/components/tools/todolist/ToolsTodoListContent.vue";
 import type { TodoList } from "@/model/todolist/TodoList.ts";
@@ -13,21 +13,27 @@ import type { TodoList } from "@/model/todolist/TodoList.ts";
 /** Att-åtgärda-listan som ska visas. */
 const todoList = defineModel<TodoList>("todoList", { required: true });
 
+const popoverVisible = ref<boolean>(false);
+
 const todoBtn = useTemplateRef("todo-btn");
 const popoverContent = useTemplateRef("popover-content");
 
+let popover: Popover | undefined;
+
 onMounted(() => {
-  if (todoBtn.value && popoverContent.value) {
-    new Popover(todoBtn.value, {
-      content: popoverContent.value,
-      html: true,
-      placement: "top",
-      offset: [0, 12],
-      trigger: "click",
-      container: "body",
-      customClass: "todo-popover",
-    });
+  if (!(todoBtn.value && popoverContent.value)) {
+    throw new Error("Missing required template refs for popover");
   }
+
+  popover = new Popover(todoBtn.value, {
+    content: popoverContent.value,
+    html: true,
+    placement: "top",
+    offset: [0, 12],
+    trigger: "manual",
+    container: "body",
+    customClass: "todo-popover",
+  });
 });
 
 const numTasksRemaining = computed(
@@ -36,17 +42,41 @@ const numTasksRemaining = computed(
       .flatMap((item) => item.tasks)
       .filter((task) => !task.complete).length,
 );
+
+watch(popoverVisible, (newVisible, oldVisible) => {
+  if (popover) {
+    if (newVisible && !oldVisible) {
+      popover.show();
+    } else if (!newVisible && oldVisible) {
+      popover.hide();
+    }
+  }
+});
 </script>
 
 <template>
   <button
     ref="todo-btn"
-    class="btn btn-outline-secondary d-flex align-items-center"
+    :class="{
+      'btn-secondary': popoverVisible,
+      'btn-outline-secondary': !popoverVisible,
+    }"
+    class="btn"
     data-testid="todo-list-button"
     type="button"
+    @click="popoverVisible = !popoverVisible"
   >
-    <i class="bi bi-list-check me-2"></i>
-    Att åtgärda
+    <span class="todo-icon-and-text-container">
+      <template v-if="popoverVisible">
+        <i class="bi bi-x-lg me-2"></i>
+        Stäng
+      </template>
+      <template v-else>
+        <i class="bi bi-list-check me-2"></i>
+        Att åtgärda
+      </template>
+    </span>
+
     <span
       v-if="numTasksRemaining > 0"
       class="badge rounded-pill bg-danger ms-2 font-monospace"
@@ -76,5 +106,11 @@ const numTasksRemaining = computed(
   --bs-popover-body-padding-y: 0;
   --bs-popover-max-width: 450px;
   --bs-popover-font-size: $font-size-base;
+}
+</style>
+
+<style lang="scss" scoped>
+.todo-icon-and-text-container {
+  min-width: 11ch;
 }
 </style>
